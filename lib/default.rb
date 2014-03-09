a75cbf4dbfb8a2022e5d798a10f309de3e507522
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2014-03-09 10:07:00 +0100

    Description updated

--- lib/default.rb
+++ lib/default.rb
@@ -6,15 +6,11 @@ include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::LinkTo
 
 def site_description(item)
-    if item[:kind] == 'article'
-        item[:description]
-    else
-        "Blog about programming and personal opensource projects such as Bonobo Git Server for Windows."
-    end
+  ".gitignore files database for your projects versioned in git"
 end
 
 def site_keywords(item)
-    keywords = "Programming, Computer Science, .NET Framework, C#, WPF, ASP.NET, Git, Bonobo, Git Server, Git Server for Windows"
+    keywords = "gitignore, .gitignore, Ignore files in git, git, gitignore windows"
     if item[:kind] == 'article'
         keywords + ", " + item[:tags].join(", ")
     else 
